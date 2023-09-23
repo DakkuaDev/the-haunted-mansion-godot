3a44e955f8fa2f66f46da9aca708815c382a7159
daniel.guerra <daniel.guerra.gallardo99@gmail.com>
2023-09-23 16:56:53 +0200

    simple door scripting logic added

--- Script/PlayerController.gd
+++ Script/PlayerController.gd
@@ -13,14 +13,10 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -30,8 +26,5 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-	
-func _on_door_body_entered(body):
-			print("body_entered event invoke!: " + body.name)
 	
 	
