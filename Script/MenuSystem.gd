e6f2ebbb1b5153ab81b27aa5944d221af2e1b51b
daniel.guerra <daniel.guerra.gallardo99@gmail.com>
2023-09-24 17:46:04 +0200

    screen game-loop logic

--- Script/MenuSystem.gd
+++ Script/MenuSystem.gd
@@ -4,11 +4,24 @@ extends Control
 @export var win_menu : TextureRect = null
 @export var main_menu : TextureRect = null
 
+var _inicialice = false;
+
+
+func _ready():
+	main_menu.visible = true
+	get_tree().paused = true
+
 
 func _process(delta):
+	
 	if (Input.is_action_just_pressed("ui_accept") && get_tree().paused == true):
-		get_tree(). reload_current_scene()
-		get_tree().paused = false
+		if(_inicialice == true):
+			get_tree(). reload_current_scene()
+		else:
+			main_menu.visible = false
+			_inicialice = true
+			
+		get_tree().paused = false			
 		
 
 func on_game_over_menu(value):
@@ -21,6 +34,8 @@ func on_game_over_menu(value):
 		game_over_menu.visible = false
 	
 func on_win_menu(value):
-	if(value): win_menu.visible = true
+	if(value): 
+		win_menu.visible = true
+		get_tree().paused = true
 	else: win_menu.visible = false
 	
---
+++ Script/TimeLeftController.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@export var initial_time_left : int = 20   
+var time_left : int = initial_time_left  
+var timer : Timer
+var text_edit  : TextEdit = null
+
+signal on_time_out
+
+func _ready():
+	# Encuentra el nodo Timer y TextEdit en tu escena
+	timer = get_node("Timer")
+	text_edit = get_node("TextEdit")
+	
+	timer.wait_time = initial_time_left
+
+	# Conecta la señal 'timeout' del temporizador a la función '_on_timer_timeout'
+	timer.connect("timeout", _on_timer_timeout, 0)
+
+	# Configura el tiempo inicial en el TextEdit
+	text_edit.text = str(time_left)
+
+	# Inicia el temporizador
+	timer.start(1)  # Empezará a contar cada segundo
+
+func _on_timer_timeout():
+	# Actualiza el tiempo restante y el TextEdit
+	time_left -= 1
+	text_edit.text = str(time_left)
+
+	# Si el tiempo llega a 0, emite la señal 'on_time_out' y detiene el temporizador
+	if time_left <= 0:
+		on_time_out.emit()
+		timer.stop()
+
